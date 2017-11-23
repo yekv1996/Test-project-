65f24eed6bce87b3fd0fb36f97711cce578d9e43
yekv1996 <qjvjqj123@gmail.com>
2017-11-23 23:47:46 +0200

    upload test project

---
+++ Task/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(123, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "600";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(38, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "500";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(38, 96);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(75, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "30";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(38, 122);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(75, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "5";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(38, 148);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(75, 20);
+            this.textBox5.TabIndex = 6;
+            this.textBox5.Text = "100";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "R";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "P";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "V";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 261);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Test task";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Task/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Task
+{
+    public partial class Form1 : Form
+    {
+        int A = 600, B = 500, R, V, P;
+        Point[] Pts;
+        Point CL;
+        public Form1()
+        {
+            InitializeComponent();
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            
+            panel1.Location = new Point(150, 0);
+           
+            this.Size = new Size(A, B);
+        }
+
+        //Start button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try { A = Convert.ToInt32(textBox1.Text); } catch { }; //Window width
+            try { B = Convert.ToInt32(textBox2.Text); } catch { }; //Window height
+            try { R= Convert.ToInt32(textBox3.Text); } catch { }; //Circle radius
+            try { P = Convert.ToInt32(textBox4.Text); } catch { }; //Number of points
+            try { V = Convert.ToInt32(textBox5.Text); } catch { }; //Circle movement speed
+            this.Size = new Size(A, B); //Window size
+            panel1.Size = new Size(this.Width - 150, this.Height); //Panel size
+            SetPoints(P, panel1);
+            CL = new Point(panel1.Width / 2 - R / 2, panel1.Height / 2 - R / 2); //Circle start point
+            double dx, dy;
+            double x;
+            PaintField(panel1);
+            foreach (Point pnt in Pts)
+            {
+                dx = (pnt.X - CL.X) / 10;
+                dy = (pnt.Y - CL.Y) / 10;
+                x = 10 / Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dy, 2));
+                dx *= x;
+                dy *= x;
+                while (CL != pnt)
+                {
+                    if (Math.Sqrt(Math.Pow(CL.X - pnt.X, 2) + Math.Pow(CL.Y - pnt.Y, 2)) < R)
+                    {
+                        CL = pnt;
+                    }
+                    else
+                        CL = new Point(Convert.ToInt32(CL.X + dx), Convert.ToInt32(CL.Y + dy));
+                    PaintField(panel1);
+                    Thread.Sleep(1000 / V * 10);
+
+                }
+            }                                 
+        }
+
+        void SetPoints(int p, Panel field) //Set the coordinates of points
+        {
+            Random rand = new Random();
+            Pts = new Point[p];
+            for (int i = 0; i < p; i++)
+            {
+                Pts[i] = new Point(rand.Next(R / 2 - 1, field.Width - R / 2), rand.Next(R / 2 - 1, field.Height - R / 2));
+
+            }
+        }
+       
+        void PaintField(Panel field) //Animation
+        {
+            Bitmap buf1 = new Bitmap(field.Width, field.Height);
+            using (Graphics g = Graphics.FromImage(buf1))
+            {
+                g.FillRectangle(Brushes.White, new Rectangle(0, 0, field.Width, field.Height));                
+                foreach (Point pnt in Pts)
+                    g.DrawEllipse(Pens.Red, new Rectangle(pnt.X - 2, pnt.Y - 2 , 4, 4));
+                g.DrawEllipse(Pens.Purple, new Rectangle(CL.X - R / 2, CL.Y - R / 2, R, R));               
+                field.CreateGraphics().DrawImageUnscaled(buf1, 0, 0);
+            }
+        }
+    }
+}
